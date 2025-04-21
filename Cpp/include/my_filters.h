1650801423bca1eb8868a8b4092b089bf44c083e
lu <2231625449@qq.com>
2025-04-21 22:20:04 +0800

    加入多圈计算

--- Cpp/include/my_filters.h
+++ Cpp/include/my_filters.h
@@ -171,6 +171,49 @@ private:
   int flag, where_num;
 };
 
+/* MultiCircle */
+class multiCircle {
+  private:
+    int count = 0;
+    double period_up = 0;
+    double period_down = 0;
+    double last_real = 0;
+    double offset = 0;
+  public:
+    multiCircle(double _period_up, double _period_down)
+    {
+      this->period_up = _period_up;
+      this->period_down = _period_down;
+    }
+    multiCircle(double _period)
+    {
+      this->period_up = abs(_period);
+      this->period_down = -abs(_period);
+    }
+    void set_offset(double _offset)
+    {
+      this->offset = _offset;
+    }
+    double f(double _real)
+    {
+      if ((_real - last_real) < (period_up - period_down) * -0.5)
+      {
+        count++;// 圈数上溢
+      }
+      else if ((_real - last_real) > (period_up - period_down) * 0.5)
+      {
+        count--;// 圈数下溢
+      }
+      last_real = _real;
+      return (_real + (period_up - period_down) * count - offset);
+    }
+    void restart(double _real)
+    {
+      this->last_real = _real;
+      this->count = 0;
+    }
+  };
+
 namespace SRML_Filter
 {
     /**
